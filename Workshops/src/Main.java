ebd165e50fec493a8397b145cd95a3aae6b683f0
septimium <septimiutg@gmail.com>
2023-12-07 20:21:36 +0200

    Hollow Pattern problem

--- Workshops/src/Main.java
+++ Workshops/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
+    public static void printHollowPattern(int noOfRows, int noOfcols) {
+        for(int i = 0; i<noOfRows; i++){
+            for(int j = 0; j<noOfcols; j++){
+                if (i == 0 || i == noOfRows - 1 || j == 0 || j == noOfcols - 1) {
+                    System.out.print('*');
+                } else {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!\n");
+        printHollowPattern(6, 6);
     }
 }
